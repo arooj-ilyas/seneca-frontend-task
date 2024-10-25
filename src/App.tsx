9bfa1d10da1cea78b6ff5eab980c0c432c200f11
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-10-25 14:37:52 -0400

    DELETE placeholder content from CRA

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello, World</p>
+        <h1>Toggle Slider</h1>
       </header>
     </div>
   );
