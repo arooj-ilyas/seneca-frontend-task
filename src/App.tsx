50a1af6da7c91e340e45c960c26762534b309fe0
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-10-26 09:34:58 -0400

    ADDED data for all 4 toggle options

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,21 @@ function App() {
     { label: "Cell Wall", value: "cell_wall" },
     { label: "Ribosomes", value: "ribosomes" },
   ];
+  const toggle2Options = [
+    { label: "Cytoplasm", value: "cytoplasm" },
+    { label: "Chloroplast", value: "chloroplast" },
+  ];
+  const toggle3Options = [
+    {
+      label: "Partially Permeable Membrane",
+      value: "partially_permeable_membrane",
+    },
+    { label: "Impermeable Membrane", value: "impermeable_membrane" },
+  ];
+  const toggle4Options = [
+    { label: "Cellulose", value: "cellulose" },
+    { label: "Mitochondria", value: "mitochondria" },
+  ];
 
   return (
     <div className="App-container">
@@ -20,6 +35,25 @@ function App() {
         initialValue="ribosomes"
         onChange={handleToggleChange}
       />
+      <ToggleSwitch
+        options={toggle2Options}
+        name="cell_toggle"
+        initialValue="cytoplasm"
+        onChange={handleToggleChange}
+      />
+      <ToggleSwitch
+        options={toggle3Options}
+        name="cell_toggle"
+        initialValue="partially_permeable_membrane"
+        onChange={handleToggleChange}
+      />
+      <ToggleSwitch
+        options={toggle4Options}
+        name="cell_toggle"
+        initialValue="cellulose"
+        onChange={handleToggleChange}
+      />
+      <h2 className="App-heading">The answer is correct!</h2>
     </div>
   );
 }
