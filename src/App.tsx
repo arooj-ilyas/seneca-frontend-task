f8c9a9dd2cf6bb786f0f8faf5e254653e46be456
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-10-25 23:56:03 -0400

    CREATED placeholder toggles

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import "./App.css";
 import ToggleSwitch from "./components/ToggleSwitch/ToggleSwitch";
 
 function App() {
+  const handleToggleChange = (value: string) => {
+    console.log("Selected option:", value);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Toggle Switch</h1>
-        <ToggleSwitch />
+        <h1>An animal cell contains:</h1>
+        <ToggleSwitch onChange={handleToggleChange} />
       </header>
     </div>
   );
--- src/components/ToggleSwitch/ToggleSwitch.tsx
+++ src/components/ToggleSwitch/ToggleSwitch.tsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import "./ToggleSwitch.css";
 
 interface PlaceholderProps {
-  text?: string;
-  className?: string;
+  onChange?: (value: string) => void;
 }
 
-const ToggleSwitch: React.FC<PlaceholderProps> = ({
-  text = "Loading...",
-  className,
-}) => {
-  return <div className={`placeholder ${className}`}>{text}</div>;
+const ToggleSwitch: React.FC<PlaceholderProps> = ({ onChange }) => {
+  const [selectedValue, setSelectedValue] = useState("answer1");
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSelectedValue(value);
+    if (onChange) {
+      onChange(value);
+    }
+  };
+
+  return (
+    <div className="toggle-switch">
+      <input
+        type="radio"
+        id="switch_left"
+        name="switch_2"
+        value="answer1"
+        checked={selectedValue === "answer1"}
+        onChange={handleChange}
+      />
+      <label htmlFor="switch_left">Answer1</label>
+      <input
+        type="radio"
+        id="switch_right"
+        name="switch_2"
+        value="answer2"
+        checked={selectedValue === "answer2"}
+        onChange={handleChange}
+      />
+      <label htmlFor="switch_right">Answer2</label>
+    </div>
+  );
 };
 
 export default ToggleSwitch;
