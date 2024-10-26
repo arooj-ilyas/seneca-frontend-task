f0a46d79e19e57c81331b502f952cf91a51310a9
Arooj Ilyas <arooj-ilyas@hotmail.com>
2024-10-26 10:53:31 -0400

    UPDATED app to be uninteractive once all fields are correct

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
 
 function App() {
   const [toggleValues, setToggleValues] = useState({
-    toggle_1: "ribosomes",
+    toggle_1: "cell_wall",
     toggle_2: "cytoplasm",
     toggle_3: "partially_permeable_membrane",
     toggle_4: "cellulose",
@@ -34,46 +34,50 @@ function App() {
   }, [toggleValues]);
 
   return (
-    <div
-      className={
-        isCorrect
-          ? "App-container blue-gradient"
-          : "App-container orange-gradient"
-      }
-    >
-      <h2 className="App-heading">An animal cell contains:</h2>
-      <ToggleSwitch
-        name="toggle_1"
-        options={toggle1Options}
-        selectedValue={toggleValues.toggle_1}
-        isCorrect={isCorrect}
-        onChange={handleToggleChange}
-      />
-      <ToggleSwitch
-        name="toggle_2"
-        options={toggle2Options}
-        selectedValue={toggleValues.toggle_2}
-        isCorrect={isCorrect}
-        onChange={handleToggleChange}
-      />
-      <ToggleSwitch
-        name="toggle_3"
-        options={toggle3Options}
-        selectedValue={toggleValues.toggle_3}
-        isCorrect={isCorrect}
-        onChange={handleToggleChange}
-      />
-      <ToggleSwitch
-        name="toggle_4"
-        options={toggle4Options}
-        selectedValue={toggleValues.toggle_4}
-        isCorrect={isCorrect}
-        onChange={handleToggleChange}
-      />
-      <h2 className="App-heading">
-        {isCorrect ? "The answer is correct!" : "The answer is incorrect."}
-      </h2>
-    </div>
+    <>
+      {isCorrect && <div className="blue-gradient-overlay" />}
+
+      <div
+        className={
+          isCorrect
+            ? "App-container blue-gradient"
+            : "App-container orange-gradient"
+        }
+      >
+        <h2 className="App-heading">An animal cell contains:</h2>
+        <ToggleSwitch
+          name="toggle_1"
+          options={toggle1Options}
+          selectedValue={toggleValues.toggle_1}
+          isCorrect={isCorrect}
+          onChange={handleToggleChange}
+        />
+        <ToggleSwitch
+          name="toggle_2"
+          options={toggle2Options}
+          selectedValue={toggleValues.toggle_2}
+          isCorrect={isCorrect}
+          onChange={handleToggleChange}
+        />
+        <ToggleSwitch
+          name="toggle_3"
+          options={toggle3Options}
+          selectedValue={toggleValues.toggle_3}
+          isCorrect={isCorrect}
+          onChange={handleToggleChange}
+        />
+        <ToggleSwitch
+          name="toggle_4"
+          options={toggle4Options}
+          selectedValue={toggleValues.toggle_4}
+          isCorrect={isCorrect}
+          onChange={handleToggleChange}
+        />
+        <h2 className="App-heading">
+          {isCorrect ? "The answer is correct!" : "The answer is incorrect."}
+        </h2>
+      </div>
+    </>
   );
 }
 
